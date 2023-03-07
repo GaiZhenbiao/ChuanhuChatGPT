99d0561074858eaed34c6194b6a8ec0b40da9a6e
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-03-08 00:10:22 +0800

    修复更多符号显示

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -63,8 +63,6 @@ def parse_text(text):
                     line = line.replace(" ", "&nbsp;")
                     line = line.replace("*", "&ast;")
                     line = line.replace("_", "&lowbar;")
-                    line = line.replace("[", "&#91;")
-                    line = line.replace("]", "&#93;")
                     line = line.replace("-", "&#45;")
                     line = line.replace("#", "&#35;")
                     line = line.replace(".", "&#46;")
