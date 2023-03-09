b453000f3c1e445e38ef1e351c0f8e6cea2b3e29
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-03-09 19:09:38 +0800

    修复引号问题

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -8,7 +8,7 @@ import requests
 # import markdown
 import csv
 
-my_api_key = "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"    # 在这里输入你的 API 密钥
+my_api_key = ""    # 在这里输入你的 API 密钥
 HIDE_MY_KEY = False # 如果你想在UI中隐藏你的 API 密钥，将此值设置为 True
 
 initial_prompt = "You are a helpful assistant."
