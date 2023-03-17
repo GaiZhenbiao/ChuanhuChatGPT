abc444eb66617fda84e21ae56f7794faebd85336
unknown <793744342@qq.com>
2023-03-17 12:12:25 +0800

    fix windows.bat

--- chatgpt - windows.bat
+++ chatgpt - windows.bat
@@ -2,7 +2,7 @@
 echo Opening ChuanhuChatGPT...
 
 REM Open powershell via bat
-start powershell.exe -NoExit -Command "python path\to\ChuanhuChatbot.py"
+start powershell.exe -NoExit -Command "python ./ChuanhuChatbot.py"
 
 REM The web page can be accessed with delayed start http://127.0.0.1:7860/
 ping -n 5 127.0.0.1>nul
