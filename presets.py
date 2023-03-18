b71be455fd994c613ad1a40584a4c662c1735265
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-03-18 12:29:07 +0800

    增加对话超时到30秒

--- presets.py
+++ presets.py
@@ -83,7 +83,7 @@ ssl_error_prompt = "SSL错误，无法获取对话。" # SSL 错误
 no_apikey_msg = "API key长度不是51位，请检查是否输入正确。" # API key 长度不足 51 位
 
 max_token_streaming = 3500 # 流式对话时的最大 token 数
-timeout_streaming = 15 # 流式对话时的超时时间
+timeout_streaming = 30 # 流式对话时的超时时间
 max_token_all = 3500 # 非流式对话时的最大 token 数
 timeout_all = 200 # 非流式对话时的超时时间
 enable_streaming_option = True  # 是否启用选择选择是否实时显示回答的勾选框
