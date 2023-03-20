4b845f902cd54b95825a8564de1673cb3b8bb2f8
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-03-20 19:08:24 +0800

    调整windows启动脚本为在默认浏览器中打开

--- chatgpt - windows.bat
+++ chatgpt - windows.bat
@@ -7,8 +7,8 @@ start powershell.exe -NoExit -Command "python ./ChuanhuChatbot.py"
 REM The web page can be accessed with delayed start http://127.0.0.1:7860/
 ping -n 5 127.0.0.1>nul
 
-REM access chargpt via your browser (default microsoft edge browser)
-start microsoft-edge:http://127.0.0.1:7860/
+REM access chargpt via your default browser
+start "" "http://127.0.0.1:7860/"
 
 
 echo Finished opening ChuanhuChatGPT (http://127.0.0.1:7860/).
