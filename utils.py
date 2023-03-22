d1c8d88b564dc8ea7a731b5af53bd39b8ca15be9
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-03-22 10:05:25 +0800

    加入更多ipapi错误处理

--- utils.py
+++ utils.py
@@ -338,13 +338,19 @@ def replace_today(prompt):
 
 def get_geoip():
     response = requests.get('https://ipapi.co/json/', timeout=5)
-    data = response.json()
+    try:
+        data = response.json()
+    except:
+        data = {
+            "error": True,
+            "reason" : "连接ipapi失败"
+        }
     if "error" in data.keys():
         logging.warning(f"无法获取IP地址信息。\n{data}")
         if data['reason'] == "RateLimited":
             return f"获取IP地理位置失败，因为达到了检测IP的速率限制。聊天功能可能仍然可用，但请注意，如果您的IP地址在不受支持的地区，您可能会遇到问题。"
         else:
-            return f"获取IP地理位置失败。原因：{data['reason']}"
+            return f"获取IP地理位置失败。原因：{data['reason']}。你仍然可以使用聊天功能。"
     else:
         country = data['country_name']
         if country == "China":
