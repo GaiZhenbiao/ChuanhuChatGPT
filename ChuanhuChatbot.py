ee061c569ad96973b2df1bc1df81562a7ec5dac6
haleyc <1617335718@qq.com>
2023-03-23 11:42:18 +0800

    sleep 2000ms 玄学避免加载不完全

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -442,28 +442,33 @@ def reload_javascript():
     print("Reloading javascript...")
     js = """
     <script>
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
     window.onload = function() {
         console.log("loaded");
-        console.log(document.querySelector("#is_pinned > label > input"));
+        sleep(2000).then(() => {  // 玄学避免加载不完全
+            console.log(document.querySelector("#is_pinned > label > input"));
 
-        const is_pinned = document.querySelector("#is_pinned > label > input");
+            const is_pinned = document.querySelector("#is_pinned > label > input");
 
-        is_pinned.addEventListener("change", function(ev) {
-            const elem = document.querySelector(':root');
-            if (ev.target.checked) {
-                console.log("pinned");
-                elem.style.setProperty('--side-bar-width', '300px');
-                elem.style.setProperty('--side-bar-display', 'flex');
+            is_pinned.addEventListener("change", function(ev) {
+                const elem = document.querySelector(':root');
+                if (ev.target.checked) {
+                    console.log("pinned");
+                    elem.style.setProperty('--side-bar-width', '300px');
+                    elem.style.setProperty('--side-bar-display', 'flex');
 
-                elem.style.setProperty('--side-bar-bg', 'var(--neutral-950)');
-            } else {
-                console.log("unpinned");
-                elem.style.setProperty('--side-bar-width', '20px');
-                elem.style.setProperty('--side-bar-display', 'none');
+                    elem.style.setProperty('--side-bar-bg', 'var(--neutral-950)');
+                } else {
+                    console.log("unpinned");
+                    elem.style.setProperty('--side-bar-width', '20px');
+                    elem.style.setProperty('--side-bar-display', 'none');
 
-                elem.style.setProperty('--side-bar-bg', 'var(--primary-800)');
-            }
-            console.log(elem)
+                    elem.style.setProperty('--side-bar-bg', 'var(--primary-800)');
+                }
+
+            });
         });
     }
     </script>
