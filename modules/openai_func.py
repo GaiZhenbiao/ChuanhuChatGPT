a2436f41a4a9d3f36d96b4ce5ec507d41970776e
Philo-Sophia <xingfanxia@gmail.com>
2023-04-01 00:13:17 -0700

    fix: fix usage stats error when it's the first day of the month (#498)
    fix #497

--- modules/openai_func.py
+++ modules/openai_func.py
@@ -49,8 +49,9 @@ def get_usage(openai_api_key):
             total_used=0
             return f"**API使用情况解析失败**"
         if balance == 0:
-            last_day_of_month = datetime.datetime.now().strftime("%Y-%m-%d")
-            first_day_of_month = datetime.datetime.now().replace(day=1).strftime("%Y-%m-%d")
+            curr_time = datetime.datetime.now()
+            last_day_of_month = get_last_day_of_month(curr_time).strftime("%Y-%m-%d")
+            first_day_of_month = curr_time.replace(day=1).strftime("%Y-%m-%d")
             usage_url = f"{shared.state.usage_api_url}?start_date={first_day_of_month}&end_date={last_day_of_month}"
             try:
                 usage_data = get_billing_data(openai_api_key, usage_url)
@@ -80,3 +81,9 @@ def get_usage(openai_api_key):
     except Exception as e:
         logging.error(f"获取API使用情况失败:"+str(e))
         return standard_error_msg + error_retrieve_prompt
+
+def get_last_day_of_month(any_day):
+    # The day 28 exists in every month. 4 days later, it's always next month
+    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+    # subtracting the number of the current day brings us back one month
+    return next_month - datetime.timedelta(days=next_month.day)
