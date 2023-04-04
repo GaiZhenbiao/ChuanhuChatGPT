94f0d7d23587384474abc2472c96689144208bba
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-04-04 20:07:33 +0800

    去除免费额度显示功能（由于OpenAI API的变化，不再可用啦），只显示当月用量

--- modules/openai_func.py
+++ modules/openai_func.py
@@ -19,7 +19,7 @@ def get_billing_data(openai_api_key, billing_url):
         "Content-Type": "application/json",
         "Authorization": f"Bearer {openai_api_key}"
     }
-    
+
     timeout = timeout_all
     with retrieve_proxy():
         response = requests.get(
@@ -27,51 +27,27 @@ def get_billing_data(openai_api_key, billing_url):
             headers=headers,
             timeout=timeout,
         )
-    
+
     if response.status_code == 200:
         data = response.json()
         return data
     else:
         raise Exception(f"API request failed with status code {response.status_code}: {response.text}")
-    
+
 
 def get_usage(openai_api_key):
     try:
-        balance_data=get_billing_data(openai_api_key, shared.state.balance_api_url)
-        logging.debug(balance_data)
+        curr_time = datetime.datetime.now()
+        last_day_of_month = get_last_day_of_month(curr_time).strftime("%Y-%m-%d")
+        first_day_of_month = curr_time.replace(day=1).strftime("%Y-%m-%d")
+        usage_url = f"{shared.state.usage_api_url}?start_date={first_day_of_month}&end_date={last_day_of_month}"
         try:
-            balance = balance_data["total_available"] if balance_data["total_available"] else 0
-            total_used = balance_data["total_used"] if balance_data["total_used"] else 0
-            usage_percent = round(total_used / (total_used+balance) * 100, 2) if balance > 0 else 0
+            usage_data = get_billing_data(openai_api_key, usage_url)
         except Exception as e:
-            logging.error(f"API使用情况解析失败:"+str(e))
-            balance = 0
-            total_used=0
-            return f"**API使用情况解析失败**"
-        if balance == 0:
-            curr_time = datetime.datetime.now()
-            last_day_of_month = get_last_day_of_month(curr_time).strftime("%Y-%m-%d")
-            first_day_of_month = curr_time.replace(day=1).strftime("%Y-%m-%d")
-            usage_url = f"{shared.state.usage_api_url}?start_date={first_day_of_month}&end_date={last_day_of_month}"
-            try:
-                usage_data = get_billing_data(openai_api_key, usage_url)
-            except Exception as e:
-                logging.error(f"获取API使用情况失败:"+str(e))
-                return f"**获取API使用情况失败**"
-            usage_rounded = round(usage_data['total_usage'] / 100, 2)
-            return f"**本月使用金额** \u3000 ${usage_rounded}"
-        
-        # return f"**免费额度**（已用/余额）\u3000${total_used} / ${balance}"
-        return f"""\
-        <b>免费额度使用情况</b>
-        <div class="progress-bar">
-            <div class="progress" style="width: {usage_percent}%;">
-                <span class="progress-text">{usage_percent}%</span>
-            </div>
-        </div>
-        <div style="display: flex; justify-content: space-between;"><span>已用 ${total_used}</span><span>可用 ${balance}</span></div>
-        """
-    
+            logging.error(f"获取API使用情况失败:"+str(e))
+            return f"**获取API使用情况失败**"
+        usage_rounded = round(usage_data['total_usage'] / 100, 2)
+        return f"**本月使用金额** \u3000 ${usage_rounded}"
     except requests.exceptions.ConnectTimeout:
         status_text = standard_error_msg + connection_timeout_prompt + error_retrieve_prompt
         return status_text
