d386f8bcc135d1b9687097e0fc15b14dd4420e5f
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-06-18 17:56:01 +0800

    feat: 删除历史之后清空聊天框

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -433,7 +433,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
         show_progress=True,
     )
     historyRefreshBtn.click(**refresh_history_args)
-    historyDeleteBtn.click(delete_chat_history, [current_model, historyFileSelectDropdown, user_name], [status_display, historyFileSelectDropdown], _js='''function showConfirmationDialog(a, b, c) {
+    historyDeleteBtn.click(delete_chat_history, [current_model, historyFileSelectDropdown, user_name], [status_display, historyFileSelectDropdown, chatbot], _js='''function showConfirmationDialog(a, b, c) {
   if (b != "") {
     var result = confirm("你真的要删除 " + b + " 吗？");
     if (result) {
