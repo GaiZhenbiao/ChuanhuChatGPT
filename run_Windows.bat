9f929a01ee690ebfd9d60a30e09d0f01fa60d761
2p990i9hpral <30399899+2p990i9hpral@users.noreply.github.com>
2023-07-10 15:59:33 +0900

    change venv folder name and merge installation and run into one file (#839)
    * Add korean language set
    
    Add korean language set
    
    * Support easier install for windows users
    
    added install.bat (create venv and install requirements)
    and modify the project to use venv.
    
    * Delete Install_Windows.bat
    
    Due to maintain consistency, I merged this code to run_Windows.bat
    
    * Update run_Windows.bat
    
    Combines installation and run into one.
    
    ---------
    
    Co-authored-by: Keldos <i@keldos.me>

--- run_Windows.bat
+++ run_Windows.bat
@@ -1,5 +1,24 @@
 @echo off
 echo Opening ChuanhuChatGPT...
 
-REM Open powershell via bat
-start powershell.exe -NoExit -Command "python ./ChuanhuChatbot.py"
+if not exist "%~dp0\ChuanhuChat\Scripts" (
+    echo Creating venv...
+    python -m venv ChuanhuChat
+    
+    cd /d "%~dp0\ChuanhuChat\Scripts"
+    call activate.bat
+    
+    cd /d "%~dp0"
+    pip install -r requirements.txt
+)
+
+goto :activate_venv
+
+:launch
+%PYTHON% ChuanhuChatbot.py %*
+pause
+
+:activate_venv
+set PYTHON="%~dp0\ChuanhuChat\Scripts\Python.exe"
+echo venv %PYTHON%
+goto :launch
