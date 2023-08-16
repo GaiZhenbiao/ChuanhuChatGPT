9dacb1a9f020829875d93a37f981c52b62917210
Keldos <hi@keldos.me>
2023-08-16 23:49:43 +0800

    feat&refactor: add_classes_to_gradio_component

--- modules/overwrites.py
+++ modules/overwrites.py
@@ -70,3 +70,37 @@ def postprocess_chat_messages(
             return chat_message
         else:
             raise ValueError(f"Invalid message for Chatbot component: {chat_message}")
+
+
+
+def add_classes_to_gradio_component(comp):
+    """
+    this adds gradio-* to the component for css styling (ie gradio-button to gr.Button), as well as some others
+    code from stable-diffusion-webui <AUTOMATIC1111/stable-diffusion-webui>
+    """
+
+    comp.elem_classes = [f"gradio-{comp.get_block_name()}", *(comp.elem_classes or [])]
+
+    if getattr(comp, 'multiselect', False):
+        comp.elem_classes.append('multiselect')
+
+
+def IOComponent_init(self, *args, **kwargs):
+    res = original_IOComponent_init(self, *args, **kwargs)
+    add_classes_to_gradio_component(self)
+
+    return res
+
+original_IOComponent_init = gr.components.IOComponent.__init__
+gr.components.IOComponent.__init__ = IOComponent_init
+
+
+def BlockContext_init(self, *args, **kwargs):
+    res = original_BlockContext_init(self, *args, **kwargs)
+    add_classes_to_gradio_component(self)
+
+    return res
+
+original_BlockContext_init = gr.blocks.BlockContext.__init__
+gr.blocks.BlockContext.__init__ = BlockContext_init
+
