58a0a813728ff583a3846794c73dfa330c59ac82
Keldos <hi@keldos.me>
2023-08-17 19:19:02 +0800

    refactor: 微调python中js写法，加入部分测试js (WIP)

---
+++ web_assets/javascript/utils.js
@@ -0,0 +1,67 @@
+
+var gradioUploader = null;
+
+function testUpload(target) {
+    gradioUploader = gradioApp().querySelector("#upload-index-file > .center.flex");
+    let uploaderEvents = ["click", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop"];
+    transEventListeners(target, gradioUploader, uploaderEvents);
+}
+
+
+function transEventListeners(target, source, events) {
+    events.forEach((sourceEvent) => {
+        target.addEventListener(sourceEvent, function (targetEvent) {
+            if(targetEvent.preventDefault) targetEvent.preventDefault();
+            if(targetEvent.stopPropagation) targetEvent.stopPropagation();
+
+            source.dispatchEvent(new Event(sourceEvent, {detail: targetEvent.detail}));
+            console.log(targetEvent.detail);
+        });
+    });
+}
+
+
+/* NOTE: These reload functions are not used in the current version of the code.
+ *       From stable-diffusion-webui
+ */
+function restart_reload() {
+    document.body.innerHTML = '<h1 style="font-family:ui-monospace,monospace;margin-top:20%;color:lightgray;text-align:center;">Reloading...</h1>';
+
+    var requestPing = function () {
+        requestGet("./internal/ping", {}, function (data) {
+            location.reload();
+        }, function () {
+            setTimeout(requestPing, 500);
+        });
+    };
+
+    setTimeout(requestPing, 2000);
+
+    return [];
+}
+
+function requestGet(url, data, handler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    var args = Object.keys(data).map(function (k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+    }).join('&');
+    xhr.open("GET", url + "?" + args, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                try {
+                    var js = JSON.parse(xhr.responseText);
+                    handler(js);
+                } catch (error) {
+                    console.error(error);
+                    errorHandler();
+                }
+            } else {
+                errorHandler();
+            }
+        }
+    };
+    var js = JSON.stringify(data);
+    xhr.send(js);
+}
