d55a9fe80b3bad960078701993631e52b6876ab8
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-09-02 16:06:25 +0800

    chore: set logging level to info

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
 )
 
