a10f81350db76e8b6a840030b2dd2e8b76a0ca58
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2023-10-21 13:05:57 +0800

    chore: Popup error message dialog

--- modules/models/models.py
+++ modules/models/models.py
@@ -128,7 +128,8 @@ def get_model(
     except Exception as e:
         import traceback
         traceback.print_exc()
-        msg = f"{STANDARD_ERROR_MSG}: {e}"
+        msg = f"{STANDARD_ERROR_MSG} {e}"
+        gr.Warning(msg)
     presudo_key = hide_middle_chars(access_key)
     if original_model is not None and model is not None:
         model.history = original_model.history
