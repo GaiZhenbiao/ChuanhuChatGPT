83cf6b2b38c9e497fd1f59f9d1ac0416c75411e4
Tuchuanhuhuhu <gzblog@hdu.edu.cn>
2023-12-21 14:39:11 +0800

    bugfix: 适配GPT4-V最新对话格式

--- modules/models/OpenAIVision.py
+++ modules/models/OpenAIVision.py
@@ -277,6 +277,8 @@ class OpenAIVisionClient(BaseLLMModel):
                     if chunk_length > 6 and "delta" in chunk["choices"][0]:
                         if "finish_details" in chunk["choices"][0]:
                             finish_reason = chunk["choices"][0]["finish_details"]
+                        elif "finish_reason" in chunk["choices"][0]:
+                            finish_reason = chunk["choices"][0]["finish_reason"]
                         else:
                             finish_reason = chunk["finish_details"]
                         if finish_reason == "stop":
