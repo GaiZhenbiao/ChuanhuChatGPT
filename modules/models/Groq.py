44b68b44f3e4fc0864bd04cceb081f5a2ebdda97
Siwei Chen <chensiwei1990@gmail.com>
2024-05-12 19:48:02 -0500

    fix: incorrect argument structure in Groq_Client.init()
    when calling BaseLLMModel.init() in Groq_Client.init(), the last argument should be `config={"api_key":api_key}`, instead of `api_key=api_key`

--- modules/models/Groq.py
+++ modules/models/Groq.py
@@ -18,7 +18,13 @@ from .base_model import BaseLLMModel
 
 class Groq_Client(BaseLLMModel):
     def __init__(self, model_name, api_key, user_name="") -> None:
-        super().__init__(model_name=model_name, user=user_name, api_key=api_key)
+        super().__init__(
+            model_name=model_name, 
+            user=user_name, 
+            config={
+                "api_key": api_key
+            }
+        )
         self.client = Groq(
             api_key=os.environ.get("GROQ_API_KEY"),
             base_url=self.api_host,
